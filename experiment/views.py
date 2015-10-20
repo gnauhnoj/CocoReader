02a9e7f17d6eba6949b037dc25b0fbdb559d3e86
Jonathan Huang <jon.hc.huang@gmail.com>
2015-10-20 00:29:05 -0400

    Add get routes for getting caption choices and final image check

--- experiment/views.py
+++ experiment/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from pymongo import MongoClient
-from random import sample
+from random import sample, choice
 import json
 
 databaseName = 'coco'
@@ -11,6 +11,7 @@ db = client[databaseName]
 anns = db['annotations']
 images = db['images']
 categories = db['categories']
+captions = db['captions']
 ocr = db['ocr']
 
 
@@ -40,10 +41,13 @@ def get_random_image(request):
     rand_image_id = sample(img_ids, 1)[0]['image_id']
 
     img_anns = list(anns.find({'image_id': rand_image_id}))
+    img_ocr = list(ocr.find({'image_id': rand_image_id}))
+
     img = {}
     img['image_id'] = rand_image_id
     img['bboxes'] = []
     img['file_name'] = images.find_one({'id': rand_image_id})['file_name']
+    img['ocr'] = []
 
     for ia in img_anns:
         bb = {}
@@ -56,4 +60,67 @@ def get_random_image(request):
         bb['bbox']['h'] = ia['bbox'][3]
         img['bboxes'] += [bb]
 
+    # alternatively we can just use the same bboxes object - but need to standardize
+    # can add filters for score...
+    for io in img_ocr:
+        bb = {}
+        bb['string'] = io['utf8_string']
+        bb['score'] = io['score']
+        bb['bbox'] = {}
+        bb['bbox']['x'] = io['bbox'][0]
+        bb['bbox']['y'] = io['bbox'][1]
+        bb['bbox']['w'] = io['bbox'][2]
+        bb['bbox']['h'] = io['bbox'][3]
+        bb['bbox']['a'] = io['bbox'][4]
+        img['ocr'] += [bb]
+
     return HttpResponse(json.dumps(img))
+
+
+def get_random_caption(caps):
+    single_cap = choice(caps)
+    temp = {}
+    temp['image_id'] = single_cap['image_id']
+    temp['caption'] = single_cap['caption']
+    return temp
+
+
+def get_survey_options(request, img_id):
+    img_id = int(img_id)
+    results = []
+
+    # good caption
+    good_caps = list(captions.find({'image_id': img_id}, {'caption': 1, 'image_id': 1}))
+    results.append(get_random_caption(good_caps))
+
+    # get captions
+    cats = anns.find({'image_id': img_id}, {'category_id': 1})
+    cats = list(set([annot['category_id'] for annot in cats]))
+
+    # similar caption
+    # alternatively, take a fraction of the cats...
+    N_CAT = 2
+    sample_cats = sample(cats, N_CAT)
+    # this is not efficient, probably can do this in mongo but confusing
+    img_pool = [set([cat['image_id'] for cat in list(anns.find({'category_id': cat}, {'image_id': 1}))]) for cat in sample_cats]
+    img_int = list(set.intersection(*img_pool))
+    similar_caps_i = choice(img_int)
+    similar_caps = list(captions.find({'image_id': similar_caps_i}, {'caption': 1, 'image_id': 1}))
+    results.append(get_random_caption(similar_caps))
+
+    # bad caption
+    bad_cap_i = choice(list(anns.find({'category_id': {'$nin': cats}}, {'image_id': 1})))['image_id']
+    bad_caps = list(captions.find({'image_id': bad_cap_i}, {'caption': 1, 'image_id': 1}))
+    results.append(get_random_caption(bad_caps))
+
+    # order is [good, similar, bad]
+    # do query for imgs - need some heuristic
+    return HttpResponse(json.dumps(results))
+
+
+def get_survey_image(request, cap_id):
+    cap_id = int(cap_id)
+    img_id = captions.find_one({'id': cap_id})['image_id']
+    img_loc = images.find_one({'id': img_id})['file_name']
+    # return image location
+    return HttpResponse(json.dumps(img_loc))
