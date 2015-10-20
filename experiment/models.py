00eddd40294f7b5171f549c8b11b03e823cdab4e
Jonathan Huang <jon.hc.huang@gmail.com>
2015-10-20 00:28:51 -0400

    Comment out unused code

--- experiment/models.py
+++ experiment/models.py
@@ -3,15 +3,15 @@ from djangotoolbox.fields import ListField
 
 
 # Create your models here.
-class Annotation(models.Model):
-    models.image_id = models.IntegerField()
-    models.category_id = models.IntegerField()
-    models.id = models.IntegerField()
-    models.bbox = ListField()
+# class Annotation(models.Model):
+#     models.image_id = models.IntegerField()
+#     models.category_id = models.IntegerField()
+#     models.id = models.IntegerField()
+#     models.bbox = ListField()
 
 
-class Image(models.Model):
-    caption = models.CharField(max_length=200)
+# class Image(models.Model):
+#     caption = models.CharField(max_length=200)
 
-    def __str__(self):              # __unicode__ on Python 2
-        return self.caption
+#     def __str__(self):              # __unicode__ on Python 2
+#         return self.caption
