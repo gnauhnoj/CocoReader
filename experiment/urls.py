d1f1577e16665f574f9aeee3a3a3f2a8d7e647ae
Jonathan Huang <jon.hc.huang@gmail.com>
2015-10-29 20:22:33 -0400

    make routes specific

--- experiment/urls.py
+++ experiment/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'annotations', views.annotations, name='annotations'),
-    url(r'random$', views.get_random_image, name='random'),
-    url(r'randomocr$', views.get_random_ocr, name='randomocr'),
+    url(r'^annotations$', views.annotations, name='annotations'),
+    url(r'^random$', views.get_random_image, name='random'),
+    url(r'^randomocr$', views.get_random_ocr, name='randomocr'),
     url(r'^surveyq/(?P<img_id>\w{0,50})/$', views.get_survey_options, name='surveyq'),
     url(r'^surveyc/(?P<img_id>\w{0,50})/$', views.get_survey_image, name='surveyc'),
 ]
